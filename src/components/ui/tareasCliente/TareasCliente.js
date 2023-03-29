e186ad3cbdc810a4b29be1492dcb28ade9aeb32a
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2023-03-29 10:40:43 -0300

    se agrego iframe tareas y homepage

--- src/components/ui/tareasCliente/TareasCliente.js
+++ src/components/ui/tareasCliente/TareasCliente.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
 const TareasCliente = () => {
+
+  const PORT = window.location.port ? window.location.port : 80;
+  const PROTOCOL = window.location.protocol;
+  const HOSTNAME = window.location.hostname;
+  const URL = `${PROTOCOL}//${HOSTNAME}:${PORT}`;
+
+  //const idCli = localStorage.getItem("cliente");
+  const idCli = 2049;
+
   return (
     <>
-      <div>Tareas Cliente</div>
+      <iframe
+            loading="lazy"
+            src={`${URL}/duoc/modulos/tareas/?idCliente=${idCli}`}
+            width={"100%"}
+            // height={"770"}
+            height={"700"}
+            style={{ border: "none" }}
+            title="Tarea Cliente"
+          ></iframe>
     </>
   )
 }
