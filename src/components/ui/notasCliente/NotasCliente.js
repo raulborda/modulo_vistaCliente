23573afcb2f9e7ccc72eb8a8c17817bfda677cab
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2023-06-08 08:32:29 -0300

    se agrego ruta a modulo notas cliente

--- src/components/ui/notasCliente/NotasCliente.js
+++ src/components/ui/notasCliente/NotasCliente.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../../context/GlobalContext';
 
 const NotasCliente = () => {
+  const PORT = window.location.port ? window.location.port : 80;
+  const PROTOCOL = window.location.protocol;
+  const HOSTNAME = window.location.hostname;
+  const URL = `${PROTOCOL}//${HOSTNAME}:${PORT}`;
+
+  const {
+    idCliente,
+  } = useContext(GlobalContext);
+
   return (
     <>
-      <div>Notas Cliente</div>
+      <iframe
+            loading="lazy"
+            src={`${URL}/duoc/modulos/notas_clientes/?idC=${idCliente}`}
+            width={"100%"}
+            height={"500"}
+            style={{ border: "none" }}
+            title="Tarea Cliente"
+          ></iframe>
     </>
   )
 }
 
 export default NotasCliente;
+
