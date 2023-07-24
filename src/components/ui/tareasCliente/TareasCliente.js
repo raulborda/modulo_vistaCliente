2388acefd16c9cb70dfb55c9527e3b4f4b576829
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2023-07-24 13:09:57 -0300

    se cambio a variables dinamicas

--- src/components/ui/tareasCliente/TareasCliente.js
+++ src/components/ui/tareasCliente/TareasCliente.js
@@ -14,8 +14,8 @@ const TareasCliente = () => {
     <>
       <iframe
             loading="lazy"
-            //src={`${URL}/tati/server/tareas_dashboard/tareasCliente_new/?idCliente=${idCli}`} // para local tati
-            src={`${URL}/duoc/server/tareas_dashboard/tareasCliente_new/?idCliente=${idCli}`} // para el resto de los crm
+            src={`${URL}/tati/server/tareas_dashboard/tareasCliente_new/?idCliente=${idCli}`} // para local tati
+            //src={`${URL}/duoc/server/tareas_dashboard/tareasCliente_new/?idCliente=${idCli}`} // para el resto de los crm
             width={"100%"}
             height={"500"}
             style={{ border: "none" }}
