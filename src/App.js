b82bc2bf47eb2484c6cc4231cad8c06d69f77357
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2023-08-02 22:19:16 -0300

    se agrego data de consumo estimado usd y real usd con porcentaje

--- src/App.js
+++ src/App.js
@@ -30,17 +30,17 @@ function App() {
   const [etiquetasCli, setEtiquetasCli] = useState([])
 
   //* Id de usuario que se obtiene desde local storage
-  const idUsu = localStorage.getItem("usuario");
-  //const idUsu = 1; //.28
+  //const idUsu = localStorage.getItem("usuario");
+  const idUsu = 1; //.28
   const [usu, setUsu] = useState(idUsu);
   //* Id de cliente que se obtine desde local storage
-  const idC = localStorage.getItem("cliSelect");
+  //const idC = localStorage.getItem("cliSelect");
   // const idC = 2; // .153
   //const idC = 2773; //.28
   //const idC = 2054; //.28
   //const idC = 2052; //.28
   //const idC = 3713; //.28
-  //const idC = 2049; //.28
+  const idC = 2049; //.28
   const [idCliente, setIdCliente] = useState(idC);
 
 
--- src/components/ui/productivoCliente/ProductivoGanaderia.js
+++ src/components/ui/productivoCliente/ProductivoGanaderia.js
@@ -10,11 +10,20 @@ import EditarEncHac from "./EditarEncHac";
 export const ProductivoGanaderia = () => {
   const URL = process.env.REACT_APP_URL;
 
-  const { idCliente, drawerNewEnc, setDrawerNewEnc,drawerEditar, setDrawerEditar, actualizarEncHac, actEncHac, setActEncHac} = useContext(GlobalContext);
-
+  const {
+    idCliente,
+    drawerNewEnc,
+    setDrawerNewEnc,
+    drawerEditar,
+    setDrawerEditar,
+    actualizarEncHac,
+    actEncHac,
+    setActEncHac,
+  } = useContext(GlobalContext);
 
   const [infoHac, setInfoHac] = useState([]);
-
+  const [infoConsumo, setInfoConsumo] = useState(null);
+  const [porcentaje, setPorcentaje] = useState(0);
 
   useEffect(() => {
     const data = new FormData();
@@ -33,8 +42,33 @@ export const ProductivoGanaderia = () => {
     });
   }, [actualizarEncHac, actEncHac]);
 
-  // console.log("Encuesta Hacienda: ", infoHac);
+  useEffect(() => {
+    const data = new FormData();
+    data.append("idC", idCliente);
+    fetch(`${URL}clientView_traerRealConsumoHacienda.php`, {
+      method: "POST",
+      body: data,
+    }).then(async function (response) {
+      await response.text().then((resp) => {
+        if (resp) {
+          const data = resp;
+          const objetoData = JSON.parse(data);
+          setInfoConsumo(Number(objetoData[0]?.costo_real));
+          // Calculamos los valores numéricos de CR y CE sin el formateo
+          let CR = parseFloat(objetoData[0]?.costo_real);
+          let CE = parseFloat(infoHac[0]?.cabh_consumoestimado);
+          let porcent = ((CR / CE) * 100);
+          setPorcentaje(parseInt(Math.round(porcent)));
+        }
+      });
+    });
+  }, [actualizarEncHac, actEncHac]);
 
+  // console.log("Info Consumo: ", infoConsumo);
+  // console.log("Info Hac: ", infoHac);
+  // console.log("Porcentaje: ", porcentaje);
+
+  const displayText = ` (${porcentaje}%)`;
 
   const closeDrawerEditar = () => {
     setDrawerEditar(false);
@@ -43,7 +77,7 @@ export const ProductivoGanaderia = () => {
   const closeDrawerCrear = () => {
     setDrawerNewEnc(false);
   };
-  
+
   const columns = [
     {
       title: "Fecha",
@@ -81,16 +115,23 @@ export const ProductivoGanaderia = () => {
       key: "cria",
       align: "center",
     },
+    {
+      title: "Cons. Est. U$S",
+      dataIndex: "consumoUSD",
+      key: "consumoUSD",
+      align: "center",
+    },
   ];
 
   const dataHac = infoHac.slice(1).map((Enc, index) => ({
     key: Enc.cabh_id,
     fecha: Enc.fecha,
-    tambos: Enc.cant_tambosprod,
-    vacasordeñe: Enc.cant_tamboscab,
-    feedlot: Enc.cant_feedlot,
-    invernador: Enc.cant_invernador,
-    cria: Enc.cant_cria,
+    tambos: parseFloat(Enc.cant_tambosprod).toLocaleString("de-DE"),
+    vacasordeñe: parseFloat(Enc.cant_tamboscab).toLocaleString("de-DE"),
+    feedlot: parseFloat(Enc.cant_feedlot).toLocaleString("de-DE"),
+    invernador: parseFloat(Enc.cant_invernador).toLocaleString("de-DE"),
+    cria: parseFloat(Enc.cant_cria).toLocaleString("de-DE"),
+    consumoUSD: parseFloat(Enc.cabh_consumoestimado).toLocaleString("de-DE"),
   }));
 
   const closeIconStyle = {
@@ -98,7 +139,6 @@ export const ProductivoGanaderia = () => {
     top: "20px",
     right: "20px",
   };
-
 
   const CustomCloseIcon = ({ onClick }) => (
     <div style={closeIconStyle} onClick={onClick}>
@@ -108,15 +148,27 @@ export const ProductivoGanaderia = () => {
 
   return (
     <>
-    <div className="divBotones">
-          <Button type="primary" style={{ borderRadius:"0px", fontWeight:"500" }} onClick={() => setDrawerNewEnc(true)}><PlusOutlined/> CREAR </Button>
-          <Button type="primary" style={{ borderRadius:"0px", marginLeft:"10px", fontWeight:"500" }} onClick={() => setDrawerEditar(true)}><EditOutlined/> EDITAR </Button>
-    </div>
+      <div className="divBotones">
+        <Button
+          type="primary"
+          style={{ borderRadius: "0px", fontWeight: "500" }}
+          onClick={() => setDrawerNewEnc(true)}
+        >
+          <PlusOutlined /> CREAR{" "}
+        </Button>
+        <Button
+          type="primary"
+          style={{ borderRadius: "0px", marginLeft: "10px", fontWeight: "500" }}
+          onClick={() => setDrawerEditar(true)}
+        >
+          <EditOutlined /> EDITAR{" "}
+        </Button>
+      </div>
       <div className="card-wrapper">
         <div className="card-contadores">
           <div className="div-secundario">
             <p className="totales">
-              {infoHac.length > 0 ? infoHac[0]?.cant_tambosprod : "-"}
+              {infoHac.length > 0 ? parseFloat(infoHac[0]?.cant_tambosprod).toLocaleString("de-DE") : "-"} 
             </p>
             <p className="descripcion">TAMBOS</p>
           </div>
@@ -130,12 +182,16 @@ export const ProductivoGanaderia = () => {
           />
           <div>
             <div className="div-secundario">
-              <p className="totales">{ infoHac.length > 0 ?infoHac[0]?.cant_tamboscab : "-"}</p>
+              <p className="totales">
+                {infoHac.length > 0 ? parseFloat(infoHac[0]?.cant_tamboscab).toLocaleString("de-DE") : "-"}
+              </p>
               <p className="descripcion">VACAS ORDEÑE</p>
             </div>
-            <Divider style={{marginTop:"10px", marginBottom:"10px"}}/>
+            <Divider style={{ marginTop: "10px", marginBottom: "10px" }} />
             <div className="div-secundario">
-              <p className="totales">{ infoHac.length > 0 ?infoHac[0]?.cabh_litros : "-"}</p>
+              <p className="totales">
+                {infoHac.length > 0 ? parseFloat(infoHac[0]?.cabh_litros).toLocaleString("de-DE") : "-"}
+              </p>
               <p className="descripcion">LITROS</p>
             </div>
           </div>
@@ -148,7 +204,9 @@ export const ProductivoGanaderia = () => {
             }}
           />
           <div className="div-secundario">
-            <p className="totales">{infoHac.length > 0 ? infoHac[0]?.cant_feedlot : "-"}</p>
+            <p className="totales">
+              {infoHac.length > 0 ? parseFloat(infoHac[0]?.cant_feedlot).toLocaleString("de-DE") : "-"}
+            </p>
             <p className="descripcion">FEEDLOT</p>
           </div>
           <Divider
@@ -160,7 +218,9 @@ export const ProductivoGanaderia = () => {
             }}
           />
           <div className="div-secundario">
-            <p className="totales">{infoHac.length > 0 ? infoHac[0]?.cant_invernador : "-"}</p>
+            <p className="totales">
+              {infoHac.length > 0 ? parseFloat(infoHac[0]?.cant_invernador).toLocaleString("de-DE") : "-"}
+            </p>
             <p className="descripcion">INVERNADOR</p>
           </div>
           <Divider
@@ -172,8 +232,41 @@ export const ProductivoGanaderia = () => {
             }}
           />
           <div className="div-secundario">
-            <p className="totales">{infoHac.length > 0 ? infoHac[0]?.cant_cria : "-"}</p>
+            <p className="totales"> 
+              {infoHac.length > 0 ? parseFloat(infoHac[0]?.cant_cria).toLocaleString("de-DE") : "-"}
+            </p>
             <p className="descripcion">CRIA</p>
+          </div>
+          <Divider
+            type="vertical"
+            style={{
+              height: "100%",
+              borderColor: "#f0f0f0",
+              borderWidth: "2px",
+            }}
+          />
+          <div>
+            <div className="div-secundario">
+              <p className="totales">
+                {infoHac.length > 0 ? parseFloat(infoHac[0]?.cabh_consumoestimado).toLocaleString("de-DE") : "-"}
+              </p>
+              <p className="descripcion">CONSUMO ESTIMADO U$S</p>
+            </div>
+            <Divider style={{ marginTop: "10px", marginBottom: "10px" }} />
+            <div className="div-secundario">
+              <div style={{display:"flex", flexDirection:"row", marginLeft:"45px"}}>
+                <p className="totales">
+                  {infoConsumo > 0 ? parseFloat(infoConsumo).toLocaleString("de-DE") : "-"}
+                </p>
+                {infoHac.length > 0 &&  infoConsumo > 0 ? (
+                  <p className="descripcion" style={{ marginLeft: "10px" }}>
+                    {/* {` (${porcentaje}%)`} */}
+                    {` (${parseInt(Math.round((parseFloat(infoConsumo)/parseFloat(infoHac[0]?.cabh_consumoestimado))*100))}%)`}
+                  </p>
+                ): ("0%")}
+              </div>
+              <p className="descripcion">CONSUMO REAL U$S</p>
+            </div>
           </div>
         </div>
       </div>
@@ -194,7 +287,7 @@ export const ProductivoGanaderia = () => {
         width={320}
         closeIcon={<CustomCloseIcon />}
       >
-        <NuevaEncHac/>
+        <NuevaEncHac />
       </Drawer>
 
       <Drawer
@@ -204,7 +297,7 @@ export const ProductivoGanaderia = () => {
         width={320}
         closeIcon={<CustomCloseIcon />}
       >
-        <EditarEncHac editar={infoHac[0]}/>
+        <EditarEncHac editar={infoHac[0]} />
       </Drawer>
     </>
   );
